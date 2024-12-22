84f05fcfcbc5e1ae3b7fd4865b926c329e65abf3
Lalit <lalitnayyar@gmail.com>
2024-12-22 22:46:53 +0400

    excercise

--- OPENAI/index.js
+++ OPENAI/index.js
@@ -4,6 +4,24 @@ const openai = new OpenAI({
     dangerouslyAllowBrowser: true
 })
 
+/**
+ * Challenge:
+ * 
+ * I've pasted some output below. Try and figure out 
+ * what instructions I gave OpenAI to get that output.
+ * 
+ * On the screen, visions gleam, a tech queen's dream, 
+ * in every home's scene.
+ * Pixels dance, in a trance, shows advance, in a 
+ * high-def glance.
+ * Remote in hand, worlds expand, from sitcom land to 
+ * news that's grand.
+ * Binging shows, the excitement grows, the plot thickens 
+ * and the time just flows.
+ * From dawn till night, in colors bright, TVs light up 
+ * our life just right.
+ * **/
+
 const messages = [
     {
         role: 'system',
